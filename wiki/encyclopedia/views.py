fd3e28fc1ab44bf69f5b3d9d80cd7a377caddad9
map3101 <marcelo.postiglione@gmail.com>
2020-07-03 21:13:58 -0300

    Creating the entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,5 @@
+import markdown2
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +10,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    #Check if the entry exists to show the appropriate page
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/entry.html",{
+            "entry": "<h1>Entry not found<h1>"
+        })
+    
+    else:
+        mdfile = util.get_entry(title)
+        return render(request, "encyclopedia/entry.html",{
+            "entry": markdown2.markdown(mdfile)
+        })
